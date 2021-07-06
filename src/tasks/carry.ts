07843c0bbe16c61b40ca7cb075f06caa4466b59c
HamsterOh <mariusnouchet@gmail.com>
2021-07-06 05:17:53 +0200

    Spread and Carry energy target fix

--- src/tasks/carry.ts
+++ src/tasks/carry.ts
@@ -5,14 +5,20 @@ export function run(creep: Creep): TaskStatus {
     if (creep.store.getFreeCapacity() <= 0) return TaskStatus.COMPLETED;
 
     const room = Game.rooms[creep.memory.room];
-    let target: StructureStorage|null = null;
-    for (const storage of _.sortBy(room.sourceStorages(), s => s.store.getUsedCapacity())) {
-        if (_.filter(Game.creeps, c => c !== creep && c.memory.role === Role.CARRIER && c.memory.target === storage.id).length > 0) continue;
 
-        target = storage;
-        creep.memory.target = storage.id;
+    const possibleStorages = _.sortBy(room.sourceStorages(), s => s.store.getUsedCapacity());
+
+    let target: StructureStorage|null = _.filter(possibleStorages, s => s.id === creep.memory.target)[0];
+    if (!target) {
+        for (const storage of possibleStorages) {
+            if (_.filter(Game.creeps, c => c !== creep && c.memory.role === Role.CARRIER && c.memory.target === storage.id).length > 0) continue;
+
+            target = storage;
+            creep.memory.target = storage.id;
+        }
+
+        if (!target) return TaskStatus.FAILED;
     }
-    if (!target) return TaskStatus.FAILED;
 
     for (const resourceType of RESOURCES_ALL) {
         if (creep.withdraw(target, resourceType) === ERR_NOT_IN_RANGE)
--- src/tasks/spreadEnergy.ts
+++ src/tasks/spreadEnergy.ts
@@ -1,18 +1,23 @@
 import {TaskStatus} from "managers/tasksManager";
+import {Role} from "../managers/rolesManager";
 
 export function run(creep: Creep): TaskStatus {
     if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) return TaskStatus.COMPLETED;
 
-    let target: Creep|null = null;
-    for (const anyCreep of _.sortBy(_.filter(Game.creeps, c => c.memory.needEnergy),
-            c => c.store.getFreeCapacity(RESOURCE_ENERGY) && creep.pos.getRangeTo(c))) {
-        if (_.filter(Game.creeps, c => c !== creep && c.memory.target === anyCreep.id).length > 0) continue;
+    const possibleTargets = _.sortBy(_.filter(Game.creeps, c => c.memory.needEnergy),
+        c => c.store.getFreeCapacity(RESOURCE_ENERGY) && creep.pos.getRangeTo(c));
 
-        target = anyCreep;
-        creep.memory.target = anyCreep.id;
+    let target: Creep|null = _.filter(possibleTargets, c => c.id === creep.memory.target)[0];
+    if (!target) {
+        for (const anyCreep of possibleTargets) {
+            if (_.filter(Game.creeps, c => c !== creep && c.memory.role === Role.CARRIER && c.memory.target === anyCreep.id).length > 0) continue;
+
+            target = anyCreep;
+            creep.memory.target = anyCreep.id;
+        }
+
+        if (!target) return TaskStatus.COMPLETED;
     }
-
-    if (!target) return TaskStatus.COMPLETED;
 
     if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
         creep.travelTo(target);
