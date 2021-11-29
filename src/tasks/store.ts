700d33119c3190c2396b9ba7b04e86d08724708e
HamsterOh <mariusnouchet@gmail.com>
2021-11-29 02:04:15 +0100

    Store to the closest structure

--- src/tasks/store.ts
+++ src/tasks/store.ts
@@ -1,19 +1,18 @@
 import { TaskStatus } from "managers/tasksManager";
 
 export function run(creep: Creep): TaskStatus {
-    const room = Game.rooms[creep.memory.room];
     if (creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0) {
         creep.memory.needEnergy = true;
         return TaskStatus.COMPLETED;
     } else if (creep.store.getFreeCapacity() <= 0) creep.memory.needEnergy = false;
 
-    const storages = creep.room.find(FIND_MY_STRUCTURES, {
+    const storages = _.sortByOrder(creep.room.find(FIND_MY_STRUCTURES, {
         filter: (structure) => (structure.structureType === STRUCTURE_SPAWN ||
             structure.structureType === STRUCTURE_EXTENSION ||
             structure.structureType === STRUCTURE_STORAGE ||
             structure.structureType === STRUCTURE_TOWER) &&
             (structure as StructureStorage).store.getFreeCapacity(RESOURCE_ENERGY) > 0
-    });
+    }), s => creep.pos.getRangeTo(s), ["desc"]);
 
     if (storages.length <= 0) {
         creep.speech("📦❌");
